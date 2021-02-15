2054ceee4c4d8eae99759a92f509d0abd0a2ebd0
madandanchilby <79045191+madandanchilby@users.noreply.github.com>
2021-02-15 19:06:28 +0700

    Source Code Tugas praktikum alpro modul 2

--- Tugas praktikum alpro.py
+++ Tugas praktikum alpro.py
@@ -1,5 +1,6 @@
 #chilby madandan/71190451
-#soal Budi tertarik untuk melamar pekerjaan pada liburan semester yang akan berlangsung selama 5 minggu. Gaji yang diberikan adalah gaji per jam. Total pajak yang harus budi
+#soal Budi tertarik untuk melamar pekerjaan pada liburan semester yang akan berlangsung selama 5 minggu. Gaji 
+# yang diberikan adalah gaji per jam. Total pajak yang harus budi
 #bayarkan dari penghasilannya selama bekerja adalah 14%. Setelah membayar pajak, budi
 #menghabiskan 10% dari pendapatan bersihnya untuk membeli baju dan aksesoris yang akan
 #digunakan pada semester baru, dan 1% untuk membeli alat tulis. Setelah membeli baju, aksesoris dan alat tulis, 
@@ -18,24 +19,23 @@ gaji=int(input('gaji per jam='))
 jam_kerja=int(input('jumlah jam kerja dalam 1 minggu='))
 
 pendapatan_Budi_sebelum_membayar_pajak=waktu_kerja * jam_kerja * gaji
-
 penghasilan_bersih=pendapatan_Budi_sebelum_membayar_pajak * 0.14
 bayar_pajak= round (penghasilan_bersih,2)
 pendapatan_budi_setelah_membayar_pajak=pendapatan_Budi_sebelum_membayar_pajak-bayar_pajak
-jumlah_uang_1=pendapatan_budi_setelah_membayar_pajak * 0.1
-jumlah_uang_2=pendapatan_budi_setelah_membayar_pajak* 0.01
-sisa_uang=pendapatan_budi_setelah_membayar_pajak-(jumlah_uang_1+jumlah_uang_2)
-jumlah_uang_3=sisa_uang * 0.25
-jumlah_uang_4=jumlah_uang_3 * 0.3
-jumlah_uang_5=jumlah_uang_3 - jumlah_uang_4
+jumlah_uang_untuk_membeli_pakaian=pendapatan_budi_setelah_membayar_pajak * 0.1
+jumlah_uang_untuk_membeli_alat_tulis=pendapatan_budi_setelah_membayar_pajak* 0.01
+sisa_uang=pendapatan_budi_setelah_membayar_pajak-(jumlah_uang_untuk_membeli_pakaian + jumlah_uang_untuk_membeli_alat_tulis)
+jumlah_uang_sedekah=sisa_uang * 0.25
+jumlah_uang_yang_di_terima_anak_yatim=jumlah_uang_sedekah * 0.3
+jumlah_uang_yang_diterima_kaum_dhuafa=jumlah_uang_sedekah - jumlah_uang_yang_di_terima_anak_yatim
 
 
 print('pendapatan Budi sebelum membayar pajak=',pendapatan_Budi_sebelum_membayar_pajak)
 print('Jumlah pajak yang harus dibayar=',bayar_pajak)
 print('pendapatan budi setelah membayar pajak=',pendapatan_budi_setelah_membayar_pajak)
-print('jumlah uang untuk membeli pakaian=',jumlah_uang_1)
-print('jumlah uang untuk membeli alat tulis=',jumlah_uang_2)
+print('jumlah uang untuk membeli pakaian=',jumlah_uang_untuk_membeli_pakaian)
+print('jumlah uang untuk membeli alat tulis=',jumlah_uang_untuk_membeli_alat_tulis)
 print('sisa uang budi=',sisa_uang)
-print('jumlah uang sedekah=',jumlah_uang_3)
-print('jumlah uang yang di terima anak yatim=',jumlah_uang_4)
-print('jumlah uang yang diterima kaum dhuafa=',jumlah_uang_5)
+print('jumlah uang sedekah=',jumlah_uang_sedekah)
+print('jumlah uang yang di terima anak yatim=',jumlah_uang_yang_di_terima_anak_yatim)
+print('jumlah uang yang diterima kaum dhuafa=',jumlah_uang_yang_diterima_kaum_dhuafa)
